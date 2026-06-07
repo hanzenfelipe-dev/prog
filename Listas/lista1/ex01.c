a2b8243b44ce4b4a9fe90ceac86349d32b9e89c5
hanzenfelipe-dev <hanzen.felipe@escola.pr.gov.br>
2026-06-07 02:13:37 +0000

    alguma coisa

--- Listas/lista1/ex01.c
+++ Listas/lista1/ex01.c
@@ -3,17 +3,17 @@
 
     typedef struct {
         char nome [50];
-        int idade;
+         int idade;
         float altura;
     } pessoa;
     int main(){
         pessoa p;
-    scanf("%s",p.nome);
-    scanf("%d", &p.idade);
-    scanf("%f", &p.altura);
-      printf("Nome:%s\n", p.nome);
-      printf("Idade:%d\n", p.idade);
-      printf("Altura:%.2f\n", p.altura);
+     scanf("%s",p.nome);
+      scanf("%d", &p.idade);
+       scanf("%f", &p.altura);
+        printf("Nome:%s\n", p.nome);
+         printf("Idade:%d\n", p.idade);
+          printf("Altura:%.2f\n", p.altura);
 
     }
     
