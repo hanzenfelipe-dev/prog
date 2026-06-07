0824d6c9759849fb9db602d558cda09f78ac88d6
hanzenfelipe-dev <hanzen.felipe@escola.pr.gov.br>
2026-06-07 13:48:03 +0000

    ezi

---
+++ Aulas/aula06/ex09.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+//so copie do exercicio 07 da lista ta identico
+ float somar(float a, float b) {
+     return a + b;
+}
+
+
+ float  subtrair(float a, float b) {
+    return a - b;
+ }
+
+ float multiplicar(float a, float b)   {
+     return a * b;
+ }
+
+ float  dividir(float a, float b) {
+    if (b  == 0) {
+        printf("erro\n");
+        return 0;
+    }
+    return a / b;
+}
+
+int main() {
+    float num1, num2;
+
+    printf("digite dois numeros: ");
+     scanf("%f %f", &num1, &num2);
+
+      printf("soma = %.2f\n", somar(num1, num2));
+       printf("subtracao = %.2f\n", subtrair(num1, num2));
+        printf("multiplicacao = %.2f\n", multiplicar(num1, num2));
+
+     if (num2 != 0) {
+         printf("Divisao = %.2f\n", dividir(num1, num2));
+    }  else {
+         dividir(num1, num2);
+    }
+
+
+
+
+      return 0;
+}
