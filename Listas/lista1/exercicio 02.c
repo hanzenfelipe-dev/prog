813745868cb0ea34d18ace4b55aeb51820e755b9
hanzenfelipe-dev <hanzen.felipe@escola.pr.gov.br>
2026-06-07 02:14:30 +0000

    qualquer coisa

--- Listas/lista1/exercicio 02.c
+++ Listas/lista1/exercicio 02.c
@@ -12,17 +12,17 @@
     int main(){
         
         placar p;
-        printf("escolha o nome do time 1:");
-        scanf("%49s", p.timeum);
+         printf("escolha o nome do time 1:");
+          scanf("%49s", p.timeum);
        
         printf("escola o nome do time 2: ");
-        scanf("%49s", p.timedois);
+          scanf("%49s", p.timedois);
         
         printf("quantos gols o time 1 fez?");
-        scanf("%d", &p.gols1);
+          scanf("%d", &p.gols1);
         
         printf("quantos gols o time 2 fez?");
-        scanf("%d", &p.gols2);
+          scanf("%d", &p.gols2);
         
         if (p.gols1 > p.gols2) {
             printf("time 1 venceu");
